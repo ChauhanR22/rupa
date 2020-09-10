59cf137d4308761f5a5c7ece10aba6f485ba62f9
Rupa <Rupa @DESKTOP-GD5KDBG>
2020-09-10 19:07:02 +0530

    new change

--- src/mypkg/Mytest.java
+++ src/mypkg/Mytest.java
@@ -2,8 +2,9 @@ package mypkg;
 
 public class Mytest {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args)
+	{
+		System.out.print("hello Girls");
 
 	}
 
